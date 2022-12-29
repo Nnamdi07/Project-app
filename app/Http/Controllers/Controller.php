3da1aa08f5414c3e21de70c7e1efe5c5d0151168
Nnamdi Ijeomah <nnamdi@Nnamdis-MacBook-Pro.local>
2022-12-29 01:24:22 +0100

    database base update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,11 +10,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function add_candidate(){
-
-    }
-    public function add_position(){
-        
-    }
 }
---
+++ app/Http/Controllers/PollController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Candidate;
+use Illuminate\Http\Request;
+
+class PollController extends Controller
+{
+    public function add_candidate(Request $request){  //function to create/register a new candidate that decides to run 
+        $request->validate([
+            'name' => 'required|string',
+            'political_party_id' => 'required',
+            'position_id' => 'required',
+        ]);
+        $candidate = new Candidate();
+        $candidate->name = $request->name;
+        $candidate->political_party_id = $request->political_party_id;
+        $candidate->position_id = $request->position_id;
+
+        $candidate->save();
+
+
+        return response()->json(["data" => $candidate],201);
+    }
+    public function update_candidate_info($id,Request $request){
+        $request->validate([
+            'id'=>'required',
+            'name' => 'required|string',
+            'political_party_id' => 'required',
+            'position_id' => 'required',
+        ]);
+
+        $candidate = Candidate::where('id', $request->id)->first();
+
+        if($candidate){
+            $candidate->name = $request->name;
+            $candidate->political_party_id = $request->political_party_id;
+            $candidate->position_id = $request->position_id;
+
+            $candidate->save();
+
+            return response()->json(["data" => $candidate],200);
+        }
+        else{
+            return response()->json(["Error" => "A Candidate with the following information does not exist"],401);
+        }
+    }
+}
